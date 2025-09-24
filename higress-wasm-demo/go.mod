8d3283348d896cc0c22dfb51787a8e63c18ff81f
masquee <okayanz@outlook.com>
2025-09-24 20:05:17 +0800

    add higress-wasm-demo

---
+++ higress-wasm-demo/go.mod
@@ -0,0 +1,17 @@
+module higress-wasm-demo
+
+go 1.25.1
+
+require (
+	github.com/higress-group/proxy-wasm-go-sdk v0.0.0-20250822030947-8345453fddd0
+	github.com/higress-group/wasm-go v1.0.3-0.20250924031006-d27ddf6b79af
+	github.com/tidwall/gjson v1.18.0
+)
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/tidwall/resp v0.1.1 // indirect
+	github.com/tidwall/sjson v1.2.5 // indirect
+)
---
+++ higress-wasm-demo/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/higress-group/proxy-wasm-go-sdk/proxywasm"
+	"github.com/higress-group/proxy-wasm-go-sdk/proxywasm/types"
+	logs "github.com/higress-group/wasm-go/pkg/log"
+	"github.com/higress-group/wasm-go/pkg/wrapper"
+	"github.com/tidwall/gjson"
+)
+
+func main() {}
+
+func init() {
+	wrapper.SetCtx(
+		// 插件名称
+		"my-plugin",
+		// 为解析插件配置，设置自定义函数
+		wrapper.ParseConfigBy(parseConfig),
+		// 为处理请求头，设置自定义函数
+		wrapper.ProcessRequestHeadersBy(onHttpRequestHeaders),
+	)
+}
+
+// 自定义插件配置
+type MyConfig struct {
+	mockEnable bool
+}
+
+// 在控制台插件配置中填写的 yaml 配置会自动转换为 json，此处直接从 json 这个参数里解析配置即可
+func parseConfig(json gjson.Result, config *MyConfig, log logs.Log) error {
+	// 解析出配置，更新到 config 中
+	config.mockEnable = json.Get("mockEnable").Bool()
+	return nil
+}
+
+func onHttpRequestHeaders(ctx wrapper.HttpContext, config MyConfig, log logs.Log) types.Action {
+	proxywasm.AddHttpRequestHeader("hello", "world")
+	if config.mockEnable {
+		proxywasm.SendHttpResponse(200, nil, []byte("hello world"), -1)
+	}
+	return types.HeaderContinue
+}
