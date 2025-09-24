578a90c0805ff99e38b9c5279fb053ddee44a65b
masquee <okayanz@outlook.com>
2025-09-24 20:11:28 +0800

    add higress-wasm-demo

--- higress-wasm-demo/main.go
+++ higress-wasm-demo/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/higress-group/proxy-wasm-go-sdk/proxywasm"
 	"github.com/higress-group/proxy-wasm-go-sdk/proxywasm/types"
-	logs "github.com/higress-group/wasm-go/pkg/log"
 	"github.com/higress-group/wasm-go/pkg/wrapper"
 	"github.com/tidwall/gjson"
 )
@@ -15,9 +14,9 @@ func init() {
 		// 插件名称
 		"my-plugin",
 		// 为解析插件配置，设置自定义函数
-		wrapper.ParseConfigBy(parseConfig),
+		wrapper.ParseConfig(parseConfig),
 		// 为处理请求头，设置自定义函数
-		wrapper.ProcessRequestHeadersBy(onHttpRequestHeaders),
+		wrapper.ProcessRequestHeaders(onHttpRequestHeaders),
 	)
 }
 
@@ -27,13 +26,13 @@ type MyConfig struct {
 }
 
 // 在控制台插件配置中填写的 yaml 配置会自动转换为 json，此处直接从 json 这个参数里解析配置即可
-func parseConfig(json gjson.Result, config *MyConfig, log logs.Log) error {
+func parseConfig(json gjson.Result, config *MyConfig) error {
 	// 解析出配置，更新到 config 中
 	config.mockEnable = json.Get("mockEnable").Bool()
 	return nil
 }
 
-func onHttpRequestHeaders(ctx wrapper.HttpContext, config MyConfig, log logs.Log) types.Action {
+func onHttpRequestHeaders(ctx wrapper.HttpContext, config MyConfig) types.Action {
 	proxywasm.AddHttpRequestHeader("hello", "world")
 	if config.mockEnable {
 		proxywasm.SendHttpResponse(200, nil, []byte("hello world"), -1)
