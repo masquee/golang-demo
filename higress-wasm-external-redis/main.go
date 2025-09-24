88f1a07f65c3bba8fba4ca8fdeb68029f0f3c901
masquee <okayanz@outlook.com>
2025-09-24 20:32:51 +0800

    add higress-wasm-external-redis

---
+++ higress-wasm-external-redis/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/higress-group/proxy-wasm-go-sdk/proxywasm"
+	"github.com/higress-group/proxy-wasm-go-sdk/proxywasm/types"
+	"github.com/tidwall/gjson"
+	"github.com/tidwall/resp"
+
+	logs "github.com/higress-group/wasm-go/pkg/log"
+	"github.com/higress-group/wasm-go/pkg/wrapper"
+)
+
+func main() {}
+
+func init() {
+	wrapper.SetCtx(
+		"redis-demo",
+		wrapper.ParseConfigBy(parseConfig),
+		wrapper.ProcessRequestHeadersBy(onHttpRequestHeaders),
+		wrapper.ProcessResponseHeadersBy(onHttpResponseHeaders),
+	)
+}
+
+type RedisCallConfig struct {
+	client wrapper.RedisClient
+	qpm    int
+}
+
+func parseConfig(json gjson.Result, config *RedisCallConfig, log logs.Log) error {
+	// 带服务类型的完整 FQDN 名称，例如 my-redis.dns、redis.my-ns.svc.cluster.local
+	serviceName := json.Get("serviceName").String()
+	servicePort := json.Get("servicePort").Int()
+	if servicePort == 0 {
+		if strings.HasSuffix(serviceName, ".static") {
+			// 静态 IP 类型服务的逻辑端口是 80
+			servicePort = 80
+		} else {
+			servicePort = 6379
+		}
+	}
+	username := json.Get("username").String()
+	password := json.Get("password").String()
+	// 单位是毫秒
+	timeout := json.Get("timeout").Int()
+	if timeout == 0 {
+		timeout = 1000
+	}
+	qpm := json.Get("qpm").Int()
+	config.qpm = int(qpm)
+	config.client = wrapper.NewRedisClusterClient(wrapper.FQDNCluster{
+		FQDN: serviceName,
+		Port: servicePort,
+	})
+	return config.client.Init(username, password, timeout)
+}
+
+func onHttpRequestHeaders(ctx wrapper.HttpContext, config RedisCallConfig, log logs.Log) types.Action {
+	now := time.Now()
+	minuteAligned := now.Truncate(time.Minute)
+	timestamp := strconv.FormatInt(minuteAligned.Unix(), 10)
+	// 如果 redis api 返回的 err != nil，一般是由于网关找不到 redis 后端服务，请检查是否误删除了 redis 后端服务
+	err := config.client.Incr(timestamp, func(response resp.Value) {
+		if response.Error() != nil {
+			log.Errorf("call redis error: %v", response.Error())
+			proxywasm.ResumeHttpRequest()
+		} else {
+			ctx.SetContext("timestamp", timestamp)
+			ctx.SetContext("callTimeLeft", strconv.Itoa(config.qpm-response.Integer()))
+			if response.Integer() == 1 {
+				err := config.client.Expire(timestamp, 60, func(response resp.Value) {
+					if response.Error() != nil {
+						log.Errorf("call redis error: %v", response.Error())
+					}
+					proxywasm.ResumeHttpRequest()
+				})
+				if err != nil {
+					log.Errorf("Error occured while calling redis, it seems cannot find the redis cluster.")
+					proxywasm.ResumeHttpRequest()
+				}
+			} else {
+				if response.Integer() > config.qpm {
+					proxywasm.SendHttpResponse(429, [][2]string{{"timestamp", timestamp}, {"callTimeLeft", "0"}}, []byte("Too many requests\n"), -1)
+				} else {
+					proxywasm.ResumeHttpRequest()
+				}
+			}
+		}
+	})
+	if err != nil {
+		// 由于调用 redis 失败，放行请求，记录日志
+		log.Errorf("Error occured while calling redis, it seems cannot find the redis cluster.")
+		return types.HeaderContinue
+	} else {
+		// 请求 hold 住，等待 redis 调用完成
+		return types.HeaderStopAllIterationAndWatermark
+	}
+}
+
+func onHttpResponseHeaders(ctx wrapper.HttpContext, config RedisCallConfig, log logs.Log) types.Action {
+	if ctx.GetContext("timestamp") != nil {
+		proxywasm.AddHttpResponseHeader("timestamp", ctx.GetContext("timestamp").(string))
+	}
+	if ctx.GetContext("callTimeLeft") != nil {
+		proxywasm.AddHttpResponseHeader("callTimeLeft", ctx.GetContext("callTimeLeft").(string))
+	}
+	return types.HeaderContinue
+}
